8bc1a09a5da2e90d2f60168399651606f5ebcd13
DHMFU <dudka.vasia2010@gmail.com>
2017-10-22 23:28:36 +0300

    lyrics parse init

--- app/routes/songs.js
+++ app/routes/songs.js
@@ -4,6 +4,19 @@ const ffmetadata = require("ffmetadata");
 const _ = require('underscore');
 
 
+// new Promise(function(resolve, reject) {
+//     request(`https://www.amalgama-lab.com/songs/${data.artist[0].toLowerCase()}/blink_182/misery.html`, function (error, response, body) {
+//         if(error) reject(error);
+//         ret = body.match(/<div class="original">(\w|\W)*?<\/div>/ig);
+//         ret = ret.map(line => line.replace(/(<div class="original">)|(<\/div>)/g, "").trim());
+//         resolve(ret);
+//     });
+// }).then(result => {
+//     if(index==files.length) {
+//         return res.json(songs);
+//     }
+// }).catch(err => console.log(err));
+
 module.exports = (app, originalPath) => {
 
     app.get('/api/songs', (req, res, next) => {
@@ -22,6 +35,10 @@ module.exports = (app, originalPath) => {
                                 _.pick(data, necessaryKeys),
                                 {filename: file, year: data.date}), 'date');
                             songs.push(data);
+                            const letter = data.artist[0].toLowerCase();
+                            const artist = data.artist.toLowerCase().replace(/\W/g,'_');
+                            const title = data.title.toLowerCase().replace(/\W/g,'_');
+                            console.log(`${letter}/${artist}/${title}`);
                             ++index;
                             if(index==files.length) {
                                 return res.json(songs);
